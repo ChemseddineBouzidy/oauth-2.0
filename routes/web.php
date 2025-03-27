6db5fed2a54f0537ca8bd8cd05c011a9195e6bd0
ChemseddineBouzidy <chamseddinexd@gmail.com>
2025-03-27 01:16:44 +0000

    Laravel socialite Google Authentication

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
+
+
+Route::get('/auth/google/redirect', function () {
+    return Socialite::driver('google')->redirect();
+})->name('google.login');
+
+Route::get('/auth/google/callback', function () {
+    $googleUser = Socialite::driver('google')->stateless()->user();
+
+    $user = User::updateOrCreate(
+        ['email' => $googleUser->getEmail()],
+        [
+            'nom' => $googleUser->getName(),
+            'email' => $googleUser->getEmail(),
+            'image' => $googleUser->getAvatar(),
+            'password' => bcrypt(str()->random(12))
+        ]
+    );
+
+    Auth::login($user);
+
+    return redirect('/dashboard'); // Redirection après connexion
+});
